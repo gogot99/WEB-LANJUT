950d21d0366d1a80ea6cdfae3c23ea410ce38f49
BHARA_99 <118202300030@mhs.dinus.ac.id>
2025-01-19 11:43:25 +0700

    web-lanjut
    uas

---
+++ storage/framework/views/b8b4c7307e25868617dd360251e50968116e6893.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>UAS WEB LANJUT</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+</head>
+<body>
+
+<div class="container">
+    <?php echo $__env->yieldContent('content'); ?>
+</div>
+
+</body>
+</html>
+<?php /**PATH D:\A18-WEB-LANJUT\web-lanjut\resources\views/template.blade.php ENDPATH**/ ?>
